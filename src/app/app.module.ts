9e8849c9e493aee6c14b3358b826c03d8f9cfd1a
DESKTOP-QKLA90D\enrik <henrydraymundo@gmail.com>
2018-07-15 12:43:34 -0500

    cascaron del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
+// Rutas
+import { AppRoutingModule } from './app-routing.module';
+
+// Modulos
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    UsuariosModule,
+
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
